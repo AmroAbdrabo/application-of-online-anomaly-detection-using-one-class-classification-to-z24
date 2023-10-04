252bf2645fd5d9216f345bdd9e9033b5016ccf4a
AmroAbdrabo <amro.abdrabo@gmail.com>
2023-10-04 22:33:04 +0200

    done with dataloader.py

--- Chapter3-ActiveLearning/Streaming-AL/RL/cnn.py
+++ Chapter3-ActiveLearning/Streaming-AL/RL/cnn.py
@@ -32,15 +32,49 @@ class CustomResNet(nn.Module):
         x = self.fc(x)
         return x
 
+# convert to numpy array for usage in the CNN
+def pcolormesh_to_array(quadmesh):
+    """
+    Convert the image plotted by pcolormesh on an Axes instance to a 2D numpy array.
+
+    Parameters:
+    - ax: matplotlib.axes.Axes, the Axes instance containing the pcolormesh plot.
+
+    Returns:
+    - img_array: 2D numpy array, the image array.
+    """
+
+    # Get the data from the QuadMesh
+    data_array = quadmesh.get_array().data
+
+    # Get the number of rows and columns
+    nrows = quadmesh._meshHeight  
+    ncols = quadmesh._meshWidth 
+    
+
+    # Convert the 1D array back to the original 2D shape
+    data_2d = data_array.reshape(nrows, ncols)
+
+    # Get the colormap and normalization from the QuadMesh
+    cmap = quadmesh.get_cmap()
+    norm = quadmesh.norm
+
+    # Convert the data to RGBA values
+    rgba_data = cmap(norm(data_2d))
+
+    # Create an image array by getting the RGB values
+    img_array = rgba_data[:, :, :3]
+
+    return img_array
 
 if __name__ == "__main__"
-    # function to transform an epoch of acceelration
-    def transform_accel_epoch(epoch):
+    # function to transform an epoch of acceelration of the shearr buidling
+    def transform_epoch_shearbuilding(epoch):
         # calculate the spectrogram
-        f, t_spec, Sxx = spectrogram(y, 4096)
+        fs = 4096
+        f, t_spec, Sxx = spectrogram(y, fs)
         
         # select subplot
-        plt.subplot(n_rows, n_cols, i+1)
         plt.pcolormesh(t_spec, f, 10*np.log10(Sxx), shading = 'gouraud')
 
     # Create the dataset and dataloader
